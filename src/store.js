b043ff8e0e552252e614a5e5fcc2646f93a95730
yogesh mishra <yogeshmishra667@gmail.com>
2022-10-11 20:00:08 +0530

    delete garbage file

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-//import { setupListeners } from '@reduxjs/toolkit/query'
-import fetchBookData from './features/book/apiSlice.js'
-export const store = configureStore({
-  reducer: {
-    book:fetchBookData
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-})
