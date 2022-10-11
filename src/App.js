826a3ff909d66662571790858d0dd21bfa3b4993
yogesh mishra <yogeshmishra667@gmail.com>
2022-10-12 00:00:13 +0530

    add about  section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from './components/about';
 import Hero from './components/hero';
 import Navbar from './components/Navbar';
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
