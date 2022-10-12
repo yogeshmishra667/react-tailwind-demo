2f0066c85de6671c6d1d5a07bd9009cd96bcc66a
yogesh mishra <yogeshmishra667@gmail.com>
2022-10-12 10:33:45 +0530

    fix menu not close while scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { Link, animateScroll as scroll } from 'react-scroll';
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
+  const handleClose = () => setNav(!nav);
 
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
@@ -53,27 +54,51 @@ const Navbar = () => {
       </div>
       <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link to="home" smooth={true} duration={500}>
+          <Link onClick={handleClose} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link to="about" smooth={true} offset={-200} duration={500}>
+          <Link
+            onClick={handleClose}
+            to="about"
+            smooth={true}
+            offset={-200}
+            duration={500}
+          >
             About
           </Link>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link to="support" smooth={true} offset={-50} duration={500}>
+          <Link
+            onClick={handleClose}
+            to="support"
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
             Support
           </Link>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link to="platforms" smooth={true} offset={-100} duration={500}>
+          <Link
+            onClick={handleClose}
+            to="platforms"
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
             Platforms
           </Link>
         </li>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link to="pricing" smooth={true} offset={-50} duration={500}>
+          <Link
+            onClick={handleClose}
+            to="pricing"
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
             Pricing
           </Link>
         </li>
